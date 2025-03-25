83eb3707df37dca9c4a8bbf9f562231a6c2669ad
Júlio Arrnaud <julio.arnaud.ia@gmail.com>
2025-03-25 15:39:11 -0300

    Update app_postgres.py
    Adicionada variáveis ambientais

--- app_postgres.py
+++ app_postgres.py
@@ -8,10 +8,11 @@ import os
 app = Flask(__name__)
 
 # Configuração do banco de dados
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "admin")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_NAME = os.getenv("DB_NAME", "collector")
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_HOST = os.environ.get('DB_HOST')
+DB_NAME = os.environ.get('DB_NAME')
+DB_PORT=os.environ.get('DB_PORT')
 
 app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
